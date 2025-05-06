94f6a58443eac436008d64f9c5a85f762747a258
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-05-06 07:44:56 -0700

    creating if conditional statement

--- mytodo/src/Todolist.js
+++ mytodo/src/Todolist.js
@@ -1,22 +1,33 @@
-import { useState } from 'react'
+import { useState } from "react"
 function Todolist(){
     const[input, setInput]=useState("")
     const[tasks, setTasks]=useState([])
     const[count, setCount]=useState(0)
+    const[error, setError]=useState("")
+    const maxTasks=10;
 
     function addTasks(){
         if(input !==""){
-        setTasks(...tasks, input)
+        setTasks([...tasks, input])
         setCount(count+1)
         setInput("")
     }
 }
+    if(input==""){
+        setError("Task is required");
+        return;
+    }
+    if(tasks.length>=maxTasks){
+        setError(`You can only add up to ${maxTasks} tasks`)
+    }
+
     return(
         <div>
-            <input placeholder="Enter Task"
-            value={tasks}
-            type="text"
-            onChange={e=>setInput(e.targetvalue)}
+            <input 
+            placeholder="Enter Task"
+            max="10"
+            value={input}
+            onChange={e=>setInput(e.target.value)}
                 />
                 <button onClick={addTasks}>Add</button>
         <ul>
